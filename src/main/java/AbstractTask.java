f19a004d765caaa223dda168ce49aa0e5d802ce6
Anthony Patton <pattonspcs@gmail.com>
2025-09-15 14:02:50 -0500

    Idk twin

--- src/main/java/AbstractTask.java
+++ src/main/java/AbstractTask.java
@@ -3,42 +3,37 @@ import lombok.Setter;
 
 import java.nio.ByteBuffer;
 
+@Getter
 public abstract class AbstractTask implements Task {
 
     protected static AbstractTask deserialize(ByteBuffer data) {
-
         byte typeId = data.get();
-
         TaskRegistry registry = TaskRegistry.fromTypeId(typeId);
 
-        if(registry == null){
-            System.err.println("Unknown type Id: " + typeId);
-            return null;
-        }
-
-        return registry.getDeserializer().apply(data);
+        return registry.deserialize(data);
     }
 
-
-    @Getter @ Setter
+    @Setter
     protected String name;
-    @Getter @Setter
+    @Setter
     protected Status status;
-    @Getter
     protected String description;
+    protected int id;
 
     public AbstractTask(String name){
         this.name = name;
         this.status = Status.TODO;
+
+
     }
 
     public byte[] serialize() {
+        byte typeId = TaskRegistry.fromClass(this.getClass()).getTypeId();
         byte[] name = this.name.getBytes();
         byte[] status = this.status.name().getBytes();
         byte[] description = this.description.getBytes();
-        byte typeId = TaskRegistry.fromClass(this.getClass()).getTypeId();
 
-        ByteBuffer buffer = ByteBuffer.allocate((1 + Integer.BYTES * 3) + name.length + status.length + description.length);
+        ByteBuffer buffer = ByteBuffer.allocate(1 + (Integer.BYTES * 3) + name.length + status.length + description.length);
         buffer.put(typeId);
         buffer.putInt(name.length);
         buffer.put(name);
@@ -48,5 +43,8 @@ public abstract class AbstractTask implements Task {
         buffer.put(description);
         return buffer.array();
     }
+
+
+
 
 }
--- src/main/java/BasicTask.java
+++ src/main/java/BasicTask.java
@@ -23,45 +23,19 @@ public class BasicTask extends AbstractTask {
         return new BasicTask(nameStr, Status.valueOf(statusStr), descriptionStr);
     }
 
-
     public BasicTask(String name, Status status, String description){
         super(name);
         this.status = status;
         this.description = description;
-    }
-
-    public BasicTask(ByteBuffer buffer){
-        super(readString(buffer));
-        this.description = "Basic Task";
-    }
-
-    public static BasicTask deserialize(ByteBuffer buffer){
-        return (BasicTask) AbstractTask.deserialize(buffer);
+        this.id = 1;
     }
 
 
-
-
-    @Override
-    public int id(){
-        return 1;
-    }
 
 
     @Override
     public String toString(){
-       return "Name: " + name + "\nId: " + id() + "\nStatus: " + getStatus() + "\nDescription: " + getDescription();
+       return "Name: " + name + "\nId: " + getId() + "\nStatus: " + getStatus() + "\nDescription: " + getDescription();
     }
-
-
-    private static String readString(ByteBuffer buffer){
-        int length = buffer.getInt();
-        byte[] lengthBytes = new byte[length];
-        return new String(lengthBytes);
-    }
-
-
-
-
 
 }
